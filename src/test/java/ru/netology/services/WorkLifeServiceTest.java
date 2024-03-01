d025541aa4324dcd152df6f8bbde14d1f779e9a1
Tatyanochka <tanyasmit16@yandex.ru>
2024-03-01 21:39:06 +0300

    initial commit

---
+++ src/test/java/ru/netology/services/WorkLifeServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class WorkLifeServiceTest {
+    @ParameterizedTest
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })
+
+    public void testThresholdArePositive(int expected, int income, int expenses, int threshold) {
+        WorkLifeService service = new WorkLifeService();
+        int actual = service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+    }
+}
